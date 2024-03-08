747b650e624884681aee63ac6519f34d60889f9b
Jhemysson <131685742+JMafuso@users.noreply.github.com>
2024-03-08 11:51:00 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Checkpoint</h1>
+      <h2>Não encontrei os arquivos de exemplo</h2>
+      <img src="https://wallpapercave.com/wp/wp10755019.jpg" alt="project" />
+    </>
+  )
+}
+
+export default App
